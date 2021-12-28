5435dc045a87338e0092cc94cb35cafaa5648fa5
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2021-12-28 13:50:51 -0300

    feat(frontend): Set room_id as cookie

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,9 @@ import { RoomComponent } from './room/room.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CookieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/room/room.component.ts
+++ frontend/src/app/room/room.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-room',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.component.scss']
 })
 export class RoomComponent implements OnInit {
+  room_id!: string
 
-  constructor() { }
+  constructor(private router: ActivatedRoute, private cookieService: CookieService) { }
 
   ngOnInit(): void {
+    var room_id = this.router.snapshot.paramMap.get("room_id")
+    if (room_id != null ){
+      this.room_id = room_id
+      this.cookieService.set("room_id", this.room_id)
+    }
   }
 
 }
